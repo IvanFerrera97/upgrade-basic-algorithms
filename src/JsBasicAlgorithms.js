417f45ed6e2ce66fc5002b36a4e11a20372ac274
Ivan <ivanferrera67@gmail.com>
2022-02-28 18:11:50 +0100

    chore:add boilerplate

--- src/JsBasicAlgorithms.js
+++ src/JsBasicAlgorithms.js
@@ -1,4 +1,4 @@
-//Interaccion #1
+/* Interaccion #1
 
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 let myFavoriteHero = "Hulk";
@@ -29,3 +29,35 @@ const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: car1['baseprice'] +
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: car2['baseprice'] + globalBasePrice};
 
 console.log(car1['finalPrice'], car2['finalPrice']);
+
+//Interaccion #3
+
+let multiplication = 10 * 5;
+alert(multiplication);
+
+let division = 10 / 2;
+alert(division);
+
+let resto = 15 % 9;
+alert(resto);
+
+let  = 10;
+let z = 5;
+let x = y + z;
+
+let a = 10;
+let b = 5;
+let c = a * b;
+
+//Interaccion #4 
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers.splice(0,1, 'IRONMAN');
+console.log(avengers); 
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers.length);
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters[4]); */
