454df0a441e80afab09feaf42f4ecd88fa03ebf5
Ivan <ivanferrera67@gmail.com>
2022-02-28 17:39:01 +0100

    chore:add boilerplate

--- src/JsBasicAlgorithms.js
+++ src/JsBasicAlgorithms.js
@@ -1,3 +1,5 @@
+//Interaccion #1
+
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 let myFavoriteHero = "Hulk";
 //1.2 Crea una variable llamada x, asigna el valor 50 a ella.
@@ -6,3 +8,24 @@ let x = 50;
 let h = 5, y =10;
 //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
+
+//Interaccion #2
+const character = {name: 'Jack Sparrow', age: 25};
+
+let firstName = "John";
+let lastName = "Snow";
+let age = "24";
+
+console.log(`Soy ${firstName} ${lastname}, tengo ${edad} y me gustan los lobos`);
+
+const toy1 = {name: 'Buss myYear', price:19};
+const toy2 = {name: 'Rayo Mcking', price:29};
+const precio = toy1['price'] + toy2['price'];
+
+console.log(precio);
+
+let globalBasePrice = 25000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: car1['baseprice'] + globalBasePrice};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: car2['baseprice'] + globalBasePrice};
+
+console.log(car1['finalPrice'], car2['finalPrice']);
